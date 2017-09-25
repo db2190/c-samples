b920c2bd78cb385f79b58777270cc5b78d32a30e
djbh <dhirajbhor21@gmail.com>
2017-09-25 12:32:52 +0530

    Original thread use program

---
+++ pthread.cpp
@@ -0,0 +1,41 @@
+
+#include <stdlib.h>
+#include <pthread.h>
+
+#include <iostream>
+using namespace std;
+
+#define NUM_THREADS 50
+
+void* PrintHello(void* threadId)
+{
+	long tid;
+
+	tid = (long)threadId;
+	cout << "Thread id is : " << tid << endl;
+	pthread_exit(NULL);
+
+}
+
+int main()
+{
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	int i;
+
+	for (i = 0; i < NUM_THREADS; i++)
+	{
+		cout << "Main: Creating thread" << i << endl;
+		rc= pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+		//rc= pthread_create(&threads[i], NULL, PrintHello, i);
+		if (rc)
+		{
+			cout << "Error : Unable to create thread. " << rc << endl;
+			exit (-1);
+		}
+	}
+
+	pthread_exit(NULL);
+
+	return 0;
+}
